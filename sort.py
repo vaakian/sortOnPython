b2b205cd9390ad5f36b8cf7ef5790eccbb7d84c5
NomadJohn <30516060+NomadJohn@users.noreply.github.com>
2017-08-08 18:38:51 +0800

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,7 +1,6 @@
-list = [2,5,20,3,5,16,63,55]
 print list
 listLen=len(list)-1
-for i in range(10):
+for i in range(len(list)):
     for j in range(listLen-i):
         if(list[j]>list[j+1]):
             tmp  = list[j]
